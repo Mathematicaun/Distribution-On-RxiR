89789a69dd85b26de6a5ad35831a37e09c14ca7e
Ayoub Zahran <162514406+Mathematicaun@users.noreply.github.com>
2025-02-12 12:15:48 +0100

    Update main.py
    Note that you have latex installed locally on your computer, to use the Modern Computer font. However, if you don't remove lines [3, 4] whose use plt.rc().

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 plt.style.use('dark_background')
 #Define the figure as 'fig' and axes as 'ax'.
 fig, ax = plt.subplots()
@@ -34,9 +35,9 @@ def Seq(re, im):
     x = np.arange(0, 2*np.pi, 2*np.pi/1000)
     ax.stackplot(np.cos(x), np.sin(x), color='white')
     ax.set_aspect('equal')
-    ax.set_title('$Z_{n+1}=Z_{n}+C$')
-    ax.set_xlabel('$\\Re$')
-    ax.set_ylabel('$\\Im$')
+    ax.set_title(r'$z_{n+1}=z_{n}+c$')
+    ax.set_xlabel(r'$\Re$')
+    ax.set_ylabel(r'$\Im$')
     ax.set(xlim=[xmin, xmax + steps('x')], ylim=[ymin, ymax + steps('y')], 
            xticks=np.arange(xmin, xmax + steps('x'), steps('x')), 
            yticks=np.arange(ymin, ymax + steps('y'), steps('y')))
@@ -45,16 +46,16 @@ def Seq(re, im):
         ax.spines[spine].set(color='c', alpha=.9)
     plt.grid(which='both', color='gray', alpha=.6)
     plt.tight_layout()
-    ax.scatter([0], [0], marker='.', alpha=.4)
+    ax.scatter(0, 0, marker='.', alpha=.4)
     z = complex(re, im)
-    ax.scatter([z.real], [z.imag], color='red', marker='.', alpha=.9)
+    ax.scatter(z.real, z.imag, color='red', marker='.', alpha=.9)
     for _ in range(points):
         ax.plot([z.real, F_z(z).real], [z.imag, F_z(z).imag], color='green', alpha=.7, lw=.5)
         z = F_z(z)
-        ax.scatter([z.real], [z.imag], color='c', marker='.', alpha=1)
+        ax.scatter(z.real, z.imag, color='c', marker='.', alpha=1)
     plt.draw()
 
-#A method using pyplot from matplotlib to get the mouse's coordinates in the 'ax' object.
+# A method using pyplot from matplotlib to get the mouse's coordinates in the 'ax' object.
 def mouse(event):
     if event.inaxes is not None:
         re = event.xdata
